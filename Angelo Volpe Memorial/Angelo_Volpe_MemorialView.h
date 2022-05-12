a336852952fe554b5186aaf0b0c3c2b874677ef9
Jeff Rippy <jrippy@tntech.edu>
2022-05-11 19:38:06 -0500

    Initial Commit

---
+++ Angelo Volpe Memorial/Angelo_Volpe_MemorialView.h
@@ -0,0 +1,12 @@
+//
+//  Angelo_Volpe_MemorialView.h
+//  Angelo Volpe Memorial
+//
+//  Created by Rippy, Jeff on 5/11/22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Angelo_Volpe_MemorialView : ScreenSaverView
+
+@end
